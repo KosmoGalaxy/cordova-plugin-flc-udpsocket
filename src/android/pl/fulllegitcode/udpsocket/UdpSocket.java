a773ce6b201475d0276d7822da701f5425fc3e0a
KosmoGalaxy <tr@rds-group.pl>
2017-09-19 17:22:23 +0200

    1.0.0

---
+++ src/android/pl/fulllegitcode/udpsocket/UdpSocket.java
@@ -0,0 +1,54 @@
+package pl.fulllegitcode.udpsocket;
+
+import android.util.SparseArray;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class UdpSocket extends CordovaPlugin {
+
+    private SparseArray<DatagramSocket> _sockets = new SparseArray<>();
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        DatagramSocket socket = _getSocket(args.getInt(0));
+        if (socket != null) {
+            if ("send".equals(action)) {
+                _send(socket, args.getString(1), args.getInt(2), args.getString(3));
+                callbackContext.success();
+                return true;
+            } else if ("broadcast".equals(action)) {
+
+            }
+        }
+        callbackContext.error("error");
+        return false;
+    }
+
+    private DatagramSocket _getSocket(int id) {
+        DatagramSocket socket = _sockets.get(id);
+        if (socket == null) {
+            try {
+                socket = new DatagramSocket();
+                _sockets.put(id, socket);
+            } catch (SocketException e) {}
+        }
+        return socket;
+    }
+
+    private void _send(DatagramSocket socket, String ip, int port, String packetString) {
+        try {
+            byte[] bytes = packetString.getBytes();
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length, InetAddress.getByName(ip), port);
+            socket.send(packet);
+        } catch (Exception e) {}
+    }
+}
--- www/udpsocket.js
+++ www/udpsocket.js
@@ -1,5 +1,18 @@
 const exec = require('cordova/exec');
+var nextId = 1;
 
-function UdpSocket() {}
+function UdpSocket() {
+  this.id = nextId++;
+}
+
+UdpSocket.prototype.send = function(ip, port, packet) {
+  exec(
+    function() {},
+    function() {},
+    'FlcUdpSocket',
+    'send',
+    [this.id, ip, port, packet]
+  );
+};
 
 module.exports = UdpSocket;
